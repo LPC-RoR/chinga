ebf8c53d001944a7ecaa4c625166873553942432
Hugo Chinga G <hugo.chinga.g@gmail.com>
2021-04-26 19:12:37 -0400

    indices

--- app/controllers/busqueda/ind_facetas_controller.rb
+++ app/controllers/busqueda/ind_facetas_controller.rb
@@ -10,7 +10,7 @@ class Busqueda::IndFacetasController < ApplicationController
 
   # GET /ind_facetas/1 or /ind_facetas/1.json
   def show
-    elementos_ids = @objeto.ind_indices.where(class_name: 'Elemento')
+    elementos_ids = @objeto.ind_indices.where(class_name: 'Elemento').map {|indice| indice.objeto_id}
     @coleccion = {}
     @coleccion['elementos'] = Elemento.where(id: elementos_ids).page(params[:page])
   end
