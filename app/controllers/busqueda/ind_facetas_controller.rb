29f0abb21b4ed30c89095a5433241d3ba1d7c9db
Hugo Chinga G <hugo.chinga.g@gmail.com>
2021-04-26 19:08:02 -0400

    temas ayuda

--- app/controllers/busqueda/ind_facetas_controller.rb
+++ app/controllers/busqueda/ind_facetas_controller.rb
@@ -1,4 +1,6 @@
 class Busqueda::IndFacetasController < ApplicationController
+  before_action :inicia_sesion
+  before_action :carga_temas_ayuda
   before_action :set_ind_faceta, only: %i[ show edit update destroy ]
 
   # GET /ind_facetas or /ind_facetas.json
